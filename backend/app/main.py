3f835bda9d622b3fedaf6b7cc9dc914827ca7345
Cameron <camnowil96@gmail.com>
2025-03-19 19:34:47 -0400

    Updated Carousel and App modules to connect to fastAPI

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ region = os.getenv("AWS_REGION")
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], # double check this 
+    allow_origins=["http://localhost:5173"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
